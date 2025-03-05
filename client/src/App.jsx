b8a27cc82fc1c61e0657337225184c392826923c
Luka <lukaneek@gmail.com>
2025-03-05 15:38:02 -0600

    first

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+const App = () => {
+
+  const [body, setBody] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3000", { 
+    })
+    .then((res) => {
+        console.log(res)
+        setBody(res.data)
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+},[]);
+
+  return (
+    <>
+    <h1>{body}</h1>
+    </>
+
+  )
+}
+export default App;
---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.get("/", cors(), function(req, res) {
+    return res.send("Hello World");
+});
+
+app.listen(3000, function(){
+    console.log('Listening on port 3000');
+});
