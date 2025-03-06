f58bf9dd0dadda135543df4c10270cc98aace443
Luka <lukaneek@gmail.com>
2025-03-06 14:54:26 -0600

    ff

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 
 app.get("/", cors(), function(req, res) {
-    return res.send("Hello World kristaa");
+    return res.send("Hello World kristaaa");
 });
 
 app.listen(3000, function(){
