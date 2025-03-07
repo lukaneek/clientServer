3f1985b75fa78704d7fa50f678b594bec8168ac3
Luka <lukaneek@gmail.com>
2025-03-07 13:31:32 -0600

    update

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 
 app.get("/", cors(), function(req, res) {
-    return res.send("Simple 2 ec2 instances app; the first instance running a React frontend, the second running an Express backend.");
+    return res.send("Response from the express server: Simple 2 ec2 instances app; the first instance running a React frontend, the second running an Express backend.");
 });
 
 app.listen(3000, function() {
