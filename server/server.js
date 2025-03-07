dcff7dc563319279e12ce29c98c0099cee03044f
lukaneek <86979747+lukaneek@users.noreply.github.com>
2025-03-07 12:52:29 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const cors = require("cors");
 const app = express();
 
 app.get("/", cors(), function(req, res) {
-    return res.send("Simple 2 ec2 instance react frontend with an express backend.");
+    return res.send("Simple 2 ec2 instances app; the first instance running a React frontend, the second running an Express backend.");
 });
 
-app.listen(3000, function(){
+app.listen(3000, function() {
     console.log('Listening on port 3000');
 });
